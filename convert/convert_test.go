0224a1478c3b913df6c22c01fe9d8e000764a097
ligongxiang <ligongxiang@xiaomi.com>
2019-08-07 01:03:55 +0800

    add int to 64bit str convert

--- convert/convert_test.go
+++ convert/convert_test.go
@@ -9,9 +9,24 @@ import (
 func TestInt64to32(t *testing.T) {
 	res, err := Int64to32(111111)
 	fmt.Println(reflect.TypeOf(res), err)
+	res, err = Int64to32(-111111)
+	fmt.Println(reflect.TypeOf(res), err)
 }
 
 func TestInt32to64(t *testing.T) {
 	res, err := Int32to64(666)
 	fmt.Println(reflect.TypeOf(res), err)
+	res, err = Int32to64(-666)
+	fmt.Println(reflect.TypeOf(res), err)
+}
+
+func TestInt32to64bitStr(t *testing.T) {
+	res := Int32to64bitStr(10000000)
+	fmt.Println(res)
+	res = Int32to64bitStr(60)
+	fmt.Println(res)
+	res = Int32to64bitStr(-10000000)
+	fmt.Println(res)
+	res = Int32to64bitStr(0)
+	fmt.Println(res)
 }
